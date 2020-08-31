650da5172434466b8002a75ebd4409f61f6cb56b
596847403 <596847403@qq.com>
2020-08-31 21:46:48 +0800

    除编辑功能基本完成

--- sever/admin/config.js
+++ sever/admin/config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	//=>WEB服务端口号
 	PORT: 8888,
-
 	//=>CROS跨域相关信息
 	CROS: {
 		ALLOW_ORIGIN: 'http://127.0.0.1:5500',
@@ -9,7 +8,6 @@ module.exports = {
 		HEADERS: 'Content-Type,Content-Length,Authorization, Accept,X-Requested-With,account,Authorization',
 		CREDENTIALS: true
 	},
-
 	//=>SESSION存储相关信息
 	SESSION: {
 		secret: 'ZFPX',
